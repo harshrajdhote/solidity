2f75a9e34b1ccbb850ebf1966bfbb437020c746b
harshrajdhote <dhoteharshraj@gmail.com>
2020-03-10 12:26:07 +0530

    first commit

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,12 +2,15 @@ const assert = require('assert')
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //web3 imports constructor thats why it is capital W
 const web3 = new Web3(ganache.provider()); //ganache is netwrok and web3 is interface ganeche provider is intermediatery
+//web3 is instance
+const {interface,bytecode} = require('../compile');
+//interface is js api
 /* mocha
 it - run a test and make an assertion (assertion means assumption in progrM must be valid)
 describe - groups together 'it' functions
 beforeEach - Execute some general setup code. 
 like if all it are using some functionality then add there so that all can use
-*/
+
 
 class Car{
     park(){
@@ -35,4 +38,24 @@ describe('CAR class',()=>{
     })
 
 })
-/* contract is created through transcation*/
+contract is created through transcation*/
+let accounts;
+beforeEach(async ()=>{
+    //Get a list of all accounts
+    //every function is async in web3
+     accounts = await web3.eth.getAccounts();//await becau control doesnt goes to next state without ref
+     inbox = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({data : bytecode, arguments: ['Hi there!']}) // second para is arguments to constructor of contract
+     .send({from:accounts[0],gas:'1000000'})        //gas ask gas can used to deploy the contract 
+});
+describe('Inbox',()=>{ 
+ it('deploys contract',()=>{
+    console.log(inbox);
+ });
+});
+//ABI code is code js to run contract its encoded contract in js which is here provided with interface
+//thats why we are passing interface
+/* javascript is sync , browser is a async , js code executed in sync manner
+so whenever the things like callbacks occur browser make our code async
+like with event queue when IO it take other function to execute
+nd execute call back when it is pushed on to the queue.*/
